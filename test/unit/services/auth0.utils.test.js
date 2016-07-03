72b268d87e6abb3592737b85d01c824e33774b48
Vlad <pleo12@gmail.com>
2016-07-03 17:04:29 +0300

    deleted unused $http declaration, unit tests fixed for links, re-builded

---
+++ test/unit/services/auth0.utils.test.js
@@ -0,0 +1,31 @@
+describe('Auth0 Utils', function() {
+    var authUtils;
+    beforeEach(initAuth0);
+    beforeEach(inject(function (_authUtils_) {
+        authUtils = _authUtils_;
+    }));
+
+    describe('Capitalize', function () {
+        it('should capitalize string', function () {
+           expect(authUtils.capitalize('test')).to.be.equal('Test');
+           expect(authUtils.capitalize('Test')).to.be.equal('Test');
+
+           expect(authUtils.capitalize('tEsT')).to.be.equal('Test');
+           expect(authUtils.capitalize('TeSt')).to.be.equal('Test');
+
+           expect(authUtils.capitalize('1test')).to.be.equal('1test');
+           expect(authUtils.capitalize('1Test')).to.be.equal('1test');
+        });
+
+        it('should be null when no argument supplied', function() {
+           expect(authUtils.capitalize()).to.be.equal(null);
+        });
+    });
+    
+    describe('fnName', function() {
+       it('should get function name', function () {
+          var funcTest = function test() {};
+          expect(authUtils.fnName(funcTest)).to.be.equal('test');
+       });
+    });
+});
